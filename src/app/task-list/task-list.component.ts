3d3ee2cddee9fe8cfbe8f5fcb7b32ae9c79d6869
will-developer <william_limaa@hotmail.com>
2025-02-12 10:53:52 -0300

    feat(task-list): Display tasks fetched from service in component

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../services/task.service';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-task-list',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, NgFor], // Imports FormsModule for ngModel and NgFor for *ngFor directive
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrls: ['./task-list.component.scss'],
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  tasks: any[] = []; // Array to store the tasks fetched from the service
 
+  constructor(private taskService: TaskService) {} // Inject TaskService in the constructor
+
+  ngOnInit(): void {
+    this.loadTasks(); // Call loadTasks when the component initializes
+  }
+
+  /**
+   * Loads tasks from the TaskService and updates the component's tasks array.
+   */
+  loadTasks(): void {
+    this.taskService.getTasks().subscribe((data: any) => {
+      // Subscribe to the getTasks Observable
+      this.tasks = data; // Assign the received task data to the tasks array
+    });
+  }
 }
